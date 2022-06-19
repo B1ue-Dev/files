69f022bab1279788de32a3a8fab33b4814cead2b
Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-06-20 01:39:29 +0700

    chore: Version bump to 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="interactions-files",
-    version="1.0.3",
+    version="1.1.0",
     description="An external for interactions.py, adding files to CommandContext for file sending",
     long_description=long_description,
     long_description_content_type="text/markdown",

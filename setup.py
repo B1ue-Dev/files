a3d3db887cb1da788907327fb3728d51ef2f6dcc
Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-08-02 01:21:45 +0700

    fix: Change version to post

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="interactions-files",
-    version="1.1.2",
+    version="1.1.2-post.1",
     description="An extension library for interactions.py allowing files in interaction responses.",
     long_description=long_description,
     long_description_content_type="text/markdown",
